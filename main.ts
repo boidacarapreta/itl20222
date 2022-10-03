ea5ef14ddd4d1f31e4c6e1817ae1c3aa45da2674
Ederson Torresini <etorresini@ifsc.edu.br>
2022-10-03 14:21:58 -0300

    Integrar mensagens por rádios e comandos de atuadores

--- main.ts
+++ main.ts
@@ -1,13 +1,35 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber >= bairro * 10 && receivedNumber <= bairro * 10 + 9) {
         comando = receivedNumber % (bairro * 10)
+        if (comando == 0) {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+        }
         if (comando == 1) {
             pins.digitalWritePin(DigitalPin.P0, 1)
         }
         basic.showNumber(comando)
     }
 })
+input.onButtonPressed(Button.A, function () {
+    if (bairro == 9) {
+        bairro = 1
+    } else {
+        bairro = bairro + 1
+    }
+    basic.showNumber(bairro)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(bairro * 10 + comando)
+    basic.showNumber(bairro * 10 + comando)
+})
+input.onButtonPressed(Button.B, function () {
+    comando = (comando + 1) % 10
+    basic.showNumber(comando)
+})
 let comando = 0
 let bairro = 0
+radio.setGroup(1)
+radio.setTransmitPower(7)
 bairro = 1
+comando = 0
 basic.showNumber(bairro)
