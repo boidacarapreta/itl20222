9a4182179fb80ee3db814490c3e64c16746511ad
Ederson Torresini <ederson.torresini@proton.me>
2022-10-23 22:09:26 -0300

    Adicionar função em jogo em rede

--- main.ts
+++ main.ts
@@ -1,23 +1,45 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    led.plot(receivedNumber / 10, receivedNumber % 10)
+    if (receivedNumber == 0) {
+        remoto.change(LedSpriteProperty.X, -1)
+    } else if (receivedNumber == 1) {
+        remoto.change(LedSpriteProperty.X, 1)
+    } else if (receivedNumber == 10) {
+        remoto.change(LedSpriteProperty.Y, -1)
+    } else if (receivedNumber == 11) {
+        remoto.change(LedSpriteProperty.Y, 1)
+    } else if (receivedNumber == 99) {
+        game.setScore(0)
+        game.gameOver()
+    }
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    sprite.change(LedSpriteProperty.X, -1)
+    local.change(LedSpriteProperty.X, -1)
+    testar_colisão(0)
 })
 input.onButtonPressed(Button.A, function () {
-    sprite.change(LedSpriteProperty.Y, -1)
+    local.change(LedSpriteProperty.Y, -1)
+    testar_colisão(10)
 })
+function testar_colisão (movimento: number) {
+    game.addScore(1)
+    if (local.isTouching(remoto)) {
+        radio.sendNumber(99)
+        game.gameOver()
+    } else {
+        radio.sendNumber(movimento)
+    }
+}
 input.onButtonPressed(Button.B, function () {
-    sprite.change(LedSpriteProperty.Y, 1)
+    local.change(LedSpriteProperty.Y, 1)
+    testar_colisão(11)
 })
 input.onGesture(Gesture.TiltRight, function () {
-    sprite.change(LedSpriteProperty.X, 1)
+    local.change(LedSpriteProperty.X, 1)
+    testar_colisão(1)
 })
-let sprite: game.LedSprite = null
+let remoto: game.LedSprite = null
+let local: game.LedSprite = null
 radio.setGroup(1)
 radio.setTransmitPower(7)
-sprite = game.createSprite(2, 2)
-loops.everyInterval(1000, function () {
-    pins.analogWritePin(AnalogPin.P0, (256 - input.lightLevel()) * 4)
-    radio.sendNumber(sprite.get(LedSpriteProperty.X) * 10 + sprite.get(LedSpriteProperty.Y))
-})
+local = game.createSprite(2, 2)
+remoto = game.createSprite(2, 2)
